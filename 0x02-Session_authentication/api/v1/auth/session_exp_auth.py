88aba89b7aa02d97ec0aa2bdcdb6cf527a5824eb
Thabiso <940303tabiso@gamil.com>
2023-05-04 14:25:06 +0000

    Add Seession Athentication Tasks

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
+""" Module of Session Auth Exp
 """
-API session expiration module
-"""
-
 from api.v1.auth.session_auth import SessionAuth
+from typing import Dict
 from os import getenv
 from datetime import datetime, timedelta
 
@@ -12,48 +11,60 @@ class SessionExpAuth(SessionAuth):
     """ Session Expiration """
 
     def __init__(self):
-        """ Override init method """
-
-        self.session_duration = 0
-
-        if int(getenv('SESSION_DURATION')):
-            self.session_duration = int(getenv('SESSION_DURATION'))
-
-    def create_session(self, user_id: str = None) -> str:
-        """ Creates a Session ID for user_id """
+        SESSION_DURATION = getenv('SESSION_DURATION', 0)
 
         try:
-            session_id = super().create_session(user_id)
+            SESSION_DURATION = int(SESSION_DURATION)
         except Exception:
+            SESSION_DURATION = 0
+
+        self.session_duration = SESSION_DURATION
+
+    def create_session(self, user_id=None):
+        """
+            Make a new Session and register in the class with time
+            Args:
+                user_id: Identificator of the user_id
+            Return:
+                Session ID
+        """
+        session_id = super().create_session(user_id)
+
+        if session_id is None:
             return None
 
-        session_dictionary = {
-            'user_id': user_id,
-            'created_at': datetime.now()
+        session_dictionary: Dict = {
+            "user_id": user_id,
+            "created_at": datetime.now()
         }
-
         self.user_id_by_session_id[session_id] = session_dictionary
 
         return session_id
 
-    def user_id_for_session_id(self, session_id: str = None) -> str:
-        """ Returns User ID based on Session ID """
-
-        if session_id is None or isinstance(session_id, str) is False:
+    def user_id_for_session_id(self, session_id=None):
+        """
+            Make a user ID based with time expiration
+            Args:
+                session_id: String of the session
+            Return:
+                User ID if not is expired
+        """
+        if session_id is None or\
+           session_id not in self.user_id_by_session_id.keys():
             return None
 
-        session_dict = self.user_id_by_session_id.get(session_id)
+        session_dictionary = self.user_id_by_session_id.get(session_id)
 
-        if session_dict is None or 'created_at' not in session_dict:
+        if self.session_duration <= 0 or session_dictionary is None:
+            return session_dictionary.get('user_id', None)
+
+        created_by = session_dictionary.get('created_at', None)
+        if created_by is None:
             return None
 
-        if self.session_duration <= 0:
-            return session_dict.get('user_id')
+        expired_session = created_by + timedelta(seconds=self.session_duration)
 
-        created_time = session_dict.get('created_at')
-        session_elapsed = timedelta(seconds=self.session_duration)
-
-        if created_time + session_elapsed < datetime.now():
+        if expired_session < datetime.now():
             return None
-        else:
-            return session_dict.get('user_id')
+
+        return session_dictionary.get('user_id', None)
