c88f9b5bdb0f7129d3ac680f0d0e0cc3b3c8d587
Thabiso <940303tabiso@gamil.com>
2023-05-04 19:54:48 +0000

    Add Session authentication Tasks

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -7,7 +7,6 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-from typing import List, TypeVar
 
 
 app = Flask(__name__)
@@ -22,15 +21,6 @@ if AUTH_TYPE == "auth":
 elif AUTH_TYPE == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-elif AUTH_TYPE == "session_auth":
-    from api.v1.auth.session_auth import SessionAuth
-    auth = SessionAuth()
-elif AUTH_TYPE == "session_exp_auth":
-    from api.v1.auth.session_exp_auth import SessionExpAuth
-    auth = SessionExpAuth()
-elif AUTH_TYPE == "session_db_auth":
-    from api.v1.auth.session_db_auth import SessionDBAuth
-    auth = SessionDBAuth()
 
 
 @app.errorhandler(404)
@@ -73,21 +63,16 @@ def before_request() -> str:
 
     expath = ['/api/v1/status/',
               '/api/v1/unauthorized/',
-              '/api/v1/forbidden/',
-              '/api/v1/auth_session/login/']
+              '/api/v1/forbidden/']
 
     if not (auth.require_auth(request.path, expath)):
         return
 
-    if (auth.authorization_header(request)) is None\
-       and auth.session_cookie(request) is None:
+    if (auth.authorization_header(request)) is None:
         abort(401)
 
-    current_user = auth.current_user(request)
-    if current_user is None:
+    if (auth.current_user(request)) is None:
         abort(403)
-
-    request.current_user = current_user
 
 
 if __name__ == "__main__":
